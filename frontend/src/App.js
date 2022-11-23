60697305431b696acebf35ceb0c6a7da57568229
peternabiswa <peter.nabiswa@gmail.com>
2022-11-22 20:40:59 -0800

    Frontend mentor challenge

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import { ReactComponent as IconButton } from "./Assets/images/icon-cart.svg";
+//import  MobileImage from "./Assets/images/image-product-mobile.jpg";
+import DesktopImage from "./Assets/images/image-product-desktop.jpg";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="pic">
+          <img
+            className="desktop"
+            src={DesktopImage}
+            alt="Bottle of Gabrielle Channel perfume"
+          />
+        </div>
+        <div className="description">
+          <h3>PERFUME</h3>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+
+          <>
+            <strong>$149.99 </strong>
+            <small>$169.99</small>
+          </>
+          <br></br>
+
+          <button className="checkoutButton">
+            <IconButton width="4rem" />
+            <span>Add to Cart</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
